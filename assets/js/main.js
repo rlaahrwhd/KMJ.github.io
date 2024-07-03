959c475700fb3074691e815a791d66a8f118834f
rlaahrwhd <rlaahrwhd@naver.com>
2024-07-03 19:59:28 +0900

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,23 +24,11 @@ function blog_posts() {
     let posts = [
         {
             url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
+            title: '창의융합인재양성 공모전 (한국정보통신보안윤리학회)',
         },
         {
             url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
+            title: '캡스톤디자인 (계명대학교)',
         },
     ];
 
@@ -76,18 +64,9 @@ function blog_posts() {
 
     // for the more posts link
     let post_template = `
-    <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
+    
 
-        <div class="blog-link">
 
-            <h3><a href="https://www.nagekar.com">Visit the blog for more posts</a></h3>            
-
-        </div>
-
-        <div class="blog-goto-link">
-            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-        </div>
-    </div>
     `;
 
     post_html.push(post_template);
